4b616f61d9175b339c3488b40c180b73ba880a97
Vishal Thakur <63438015+VishalThakur290@users.noreply.github.com>
2023-03-31 11:22:49 +0530

    Cors Origin URL updated with API production URL

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const PORT =  process.env.PORT || 5000;
 
 const app = express();
 app.use(cors({
-    origin: 'http://localhost:5173'
+    origin: 'https://frontend--strong-chebakia-5d96ad.netlify.app'
 }));
 app.use(bodyParser.json());
 app.use('/todos', todoRouter);
