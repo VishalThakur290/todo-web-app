3e2c0b7eba0b8074d890cdc1afb5e2dd66d00d63
Vishal Thakur <63438015+VishalThakur290@users.noreply.github.com>
2023-03-31 11:30:46 +0530

    Allowed access to Localhost:5173 and Production URL to API

--- app.js
+++ app.js
@@ -7,8 +7,10 @@ const cors = require('cors');
 const PORT =  process.env.PORT || 5000;
 
 const app = express();
+// Allow multiple URLs in CORS policy
+const allowedOrigins = ['https://frontend--strong-chebakia-5d96ad.netlify.app', 'http://localhost:5173'];
 app.use(cors({
-    origin: 'https://frontend--strong-chebakia-5d96ad.netlify.app'
+    origin: allowedOrigins
 }));
 app.use(bodyParser.json());
 app.use('/todos', todoRouter);
